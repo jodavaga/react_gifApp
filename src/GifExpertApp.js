2800c58b581cc4a8b644882ae61bc2ff71386c83
josedaniel.vasquez <josedaniel.vasquez@globant.com>
2020-07-07 17:51:33 -0500

    base project

---
+++ src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GifExpertApp = () => {
+
+    return(
+        <>
+            <h1>Gif Expert App</h1>
+            <hr />
+        </>
+    )
+}
+
+export default GifExpertApp;
