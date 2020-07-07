a97621a3ee7af510dd341cf70fc81174106c41e0
josedaniel.vasquez <josedaniel.vasquez@globant.com>
2020-07-07 18:05:56 -0500

    render components based on state. Set state using hooks

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
+
+    // const categories = ['Frutas', 'Verduras', 'Carne'];
+    const [categories, setCategories] = useState(['Frutas', 'Verduras', 'Carne']);
+
+    const addCategory = () => {
+        // setCategories( ['Pescado', ...categories] );
+        setCategories( cats => ['Pescado', ...cats] );
+    }
 
     return(
         <>
             <h1>Gif Expert App</h1>
             <hr />
+
+            <button onClick={ addCategory }>Add category</button>
+
+            <ol>
+                {
+                    categories.map(cat => {
+                        return(
+                            <li key={ cat }>
+                                { cat }
+                            </li>
+                        )
+                    })
+                }
+            </ol>
         </>
     )
 }
