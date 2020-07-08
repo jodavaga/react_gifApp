b6316ab5dec21595c4f11d02566a0b4b13f7fe2b
josedaniel.vasquez <josedaniel.vasquez@globant.com>
2020-07-08 11:23:38 -0500

    intro to useEffect hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const GifGrid = ({ category }) => {
 
+    const apiKey = 'd50nWkDCfjhEfVgJIE9WYClyFqqYuGkZ';
+
+    const [count, setCount] = useState(0);
+
     const getGifs = async () => {
 
-        const url = 'https://api.giphy.com/v1/gifs/search?q=rick+and+morty&limit=10&api_key=d50nWkDCfjhEfVgJIE9WYClyFqqYuGkZ';
+        const url = `https://api.giphy.com/v1/gifs/search?q=rick+and+morty&limit=10&api_key=${ apiKey }`;
 
         const resp = await fetch( url );
         const {data} = await resp.json();
@@ -14,19 +18,21 @@ export const GifGrid = ({ category }) => {
             return {
                 id: img.id,
                 title: img.title,
-                url: img.images.downsized_medium.url
+                url: img.images?.downsized_medium.url
             }
         })
 
         console.log(gif);
-
     }
 
+    // this call will be triggered each time
     getGifs();
 
     return(
         <>
             <h2>{category}</h2>
+            <h3>{ count }</h3>
+            <button onClick={() => setCount(count + 1)}>Add</button>
         </>
     )
 }
