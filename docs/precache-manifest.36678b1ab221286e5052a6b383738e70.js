398e0c34d7e4710d6c7e5a61fd3e54bc44cec36f
Jose D. Vasquez <josedaniel.vasquez@globant.com>
2020-07-09 11:52:58 -0500

    prepare production build

---
+++ docs/precache-manifest.36678b1ab221286e5052a6b383738e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b23a3d963b9ff40ca0c1d82ad9d713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2653f2ed78ee3324c74",
+    "url": "/static/css/main.a5509aa6.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2653f2ed78ee3324c74",
+    "url": "/static/js/main.59aee255.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
